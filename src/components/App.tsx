9d4d640e7319a1edc5b5ec553397f1609499ca80
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-08-02 17:09:16 -0300

    Add Create route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TodoContext from "../contexts/TodoContext";
+import AddTodo from "./AddTodo";
 import Navbar from "./Navbar";
 import TodoList from "./TodoList";
 
@@ -12,7 +13,7 @@ const App = () => {
         <br />
         <div className="uk-container">
           <Routes>
-            <Route path="/create"></Route>
+            <Route path="/create" element={<AddTodo />}></Route>
             <Route path="/" element={<TodoList />} />
           </Routes>
         </div>
